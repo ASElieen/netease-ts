dfeae7f32b6828f7574d0c9a1e0a18eeaeed15c8
username <790768275@qq.com>
2022-10-23 17:28:33 +0800

    完成recommend组件轮播（mock数据）

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+// import {configureStore} from '@reduxjs/toolkit'
+
+// const store = configureStore({
+//     reducer:{}
+// })
+
+
+// export type RootState = ReturnType<typeof store.getState()>
+
+// export default store
+
+export const test = ()=>{}
